d12de48ad79959f6d6ce8a53058c469125294656
unknown <saherelgendy1990@gmail.com>
2020-07-14 05:48:25 -0700

    validating sign up data

--- functions/index.js
+++ functions/index.js
@@ -25,7 +25,7 @@ const isEmpty = (string) => string.trim() === '' ? true : false;
 
 const isEmail = (email) => {
   const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if(email.match(regEx)) return true;
+  if (email.match(regEx)) return true;
   else return false;
 }
 
@@ -36,6 +36,15 @@ app.post('/signup', (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle
   }
+
+  let errors = {};
+
+  if (isEmpty(newUser.email)) errors.email = "Must not be empty";
+  if (isEmpty(newUser.password)) errors.password = "Must not be empty";
+  if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = "passwords must match";
+  if (isEmptympty(newUser.handle)) errors.handle = "Must not be empty";
+
+  if (Object.keys(errors).length > 0) res.status(400).json({ errors })
 
   let token, userId;
   db.doc(`/users/${newUser.handle}`).get().then(doc => {
@@ -64,10 +73,10 @@ app.post('/signup', (req, res) => {
       return res.status(201).json({ token });
     })
     .catch(err => {
-      if(err.code === "auth/email-already-in-use") {
-        return res.status(400).json({email: 'Email is alrready in use'})
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: 'Email is alrready in use' })
       } else {
-        return res.status(500).json({general: 'something went wrong, please try again'})
+        return res.status(500).json({ general: 'something went wrong, please try again' })
       }
     })
 })
