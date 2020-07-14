27b9a49d8764c64d9c7ffa53a08fd7f9d1069e7d
unknown <saherelgendy1990@gmail.com>
2020-07-14 05:35:09 -0700

    creating isEmail function to validate email

--- functions/index.js
+++ functions/index.js
@@ -23,6 +23,12 @@ const db = admin.firestore();
 
 const isEmpty = (string) => string.trim() === '' ? true : false;
 
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(email.match(regEx)) return true;
+  else return false;
+}
+
 app.post('/signup', (req, res) => {
   const newUser = {
     emai: req.body.email,
