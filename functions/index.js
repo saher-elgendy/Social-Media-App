cef1405bb25a7cf99703dcd9b7c8d3887dba37e3
unknown <saherelgendy1990@gmail.com>
2020-07-22 21:22:02 -0700

    adding getAuthenticatedUser function

--- functions/index.js
+++ functions/index.js
@@ -3,17 +3,19 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, signin, uploadImage, addUserDetails } = require('./handlers/users');
+const { signup, signin, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 const FBAuth = require('./util/fbAuth')
 
 //scream routes
 app.get('/screams', getAllScreams);
 app.post('/screams', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
 
 //users Route
 app.post('/signup', signup);
 app.post('/signin', signin);
-app.post('/user/image', uploadImage);
-app.post('/user', addUserDetails)
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser)
 //http://europe-west1-chat-app-ebabf.cloudfunctions.net/api
 exports.api = functions.region('europe-west1').https.onRequest(app);
