ff0e9b09eb308089a6a3ed3c0ce1f692930cf6b8
unknown <saherelgendy1990@gmail.com>
2020-07-27 20:17:26 -0700

    creating home page and fetching screams by axios

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Scream from '../components/Scream';
 
 const Home = () => {
+    const [screams, setScreams] = useState(null);
+
+    useEffect(() => {
+        axios.get('/screams')
+            .then(res => {
+                setScreams(res.data.screams);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, []);
+
     return (
-        <div>
-            <h1>home page</h1>
-        </div>
+        <Grid container spacing={2}>
+            <Grid item sm={8} xs={12}>
+                {
+                    screams ?
+                        screams.map(scream => {
+                            return (
+                                <Scream scream={scream}/>
+                            )
+                        }) : 'Loading...'
+                }
+            </Grid>
+            <Grid item sm={4} xs={12}>
+                <p>...profile</p>
+            </Grid>
+        </Grid>
     )
 }
 
