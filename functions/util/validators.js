704a72468d756bba930c28c59ad94582a5275aa7
unknown <saherelgendy1990@gmail.com>
2020-07-30 22:16:04 -0700

    fixing signup confirmPassword and hanlde fields validation

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -16,8 +16,11 @@ exports.validateSignupData = (data) => {
     else if (!isEmail(data.email)) errors.email = 'Must be a valid email address';
 
     if (isEmpty(data.password)) errors.password = 'Must not be empty';
-    if (data.confirmPassword !== data.password) errors.confirmPassword = 'Passwords must match';
 
+    if(isEmpty(data.confirmPassword)) errors.confirmPassword = 'Must not be empty';
+    else if (data.confirmPassword !== data.password) errors.confirmPassword = 'Passwords must match';
+
+    if(isEmpty(data.handle)) errors.handle = 'Must not be empty';
     return {
         errors,
         valid: Object.keys(errors).length === 0 ? true : false
