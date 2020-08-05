1419d60cb359321d4c46e804c28e810f54c1706c
unknown <saherelgendy1990@gmail.com>
2020-08-04 22:02:32 -0700

    connecting component to store and implementing proptypes

--- src/util/AuthRoute.js
+++ src/util/AuthRoute.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 
 const AuthRoute = ({ component: Component, authenticated, ...rest }) => {
+    console.log(authenticated)
     return (
         <Route
             render={props => authenticated ? <Redirect to="/" /> : <Component {...props} />}
@@ -10,4 +14,11 @@ const AuthRoute = ({ component: Component, authenticated, ...rest }) => {
     )
 }
 
-export default AuthRoute;
+AuthRoute.propTypes = {
+    authenticated: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+    authenticated: state.user.authenticated
+});
+export default connect(mapStateToProps)(AuthRoute);
