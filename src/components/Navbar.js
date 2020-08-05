1f0f4d7ed906191ce90195301ab0a9a0729d5f38
unknown <saherelgendy1990@gmail.com>
2020-08-04 21:57:02 -0700

    creatingnavbar logout link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/toolbar';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from './../redux/actions/userActions';
 
 const useStyles = makeStyles({
     bar: {
@@ -17,7 +19,7 @@ const useStyles = makeStyles({
     }
 })
 
-const Navbar = () => {
+const Navbar = ({ logout }) => {
     const classes = useStyles();
 
     return (
@@ -26,9 +28,15 @@ const Navbar = () => {
                 <Button color="inherit" component={Link} to="/">Home</Button>
                 <Button color="inherit" component={Link} to="/login">Login</Button>
                 <Button color="inherit" component={Link} to="/signup">Signup</Button>
+                <Button
+                    color="inherit"
+                    component={Link}
+                    to="/login"
+                    onClick={logout}
+                >Logout</Button>
             </Toolbar>
         </AppBar>
     )
 }
 
-export default Navbar;
+export default connect(null, { logout })(Navbar);
