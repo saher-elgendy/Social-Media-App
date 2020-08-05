8b6d56dc907c6513c5503f64764ffdb9531daf60
unknown <saherelgendy1990@gmail.com>
2020-08-04 22:03:00 -0700

    adding profile style to theme

--- src/util/theme.js
+++ src/util/theme.js
@@ -7,44 +7,89 @@ export default {
       contrastText: '#fff'
     },
     secondary: {
-      main: '#004d40',
+      main: '#f50057',
       contrastText: '#fff'
     }
   },
   spread: {
-     typography: {
-    userNextVariants: true
-  },
-  form: {
-    textAlign: 'center',
-  },
-  img: {
-    maxWidth: 60,
-    margin: '1.5rem auto 5px'
-  },
-  textField: {
-    margin: 20
-  },
-  button: {
-    marginTop: 16,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 'auto',
-    width: 90,
-    height: 50
-  },
-  customError: {
-    color: '#FF0000',
-    fontSize: '0.7rem'
-  },
-  small: {
-    display: 'block',
-    marginTop: '1rem'
-  },
-  circularProgress: {
-    color: '#fff',
-    position: 'absolute'
-  }
+    typography: {
+      userNextVariants: true
+    },
+    form: {
+      textAlign: 'center',
+    },
+    img: {
+      maxWidth: 60,
+      margin: '1.5rem auto 5px'
+    },
+    textField: {
+      margin: 20
+    },
+    button: {
+      marginTop: 16,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      margin: 'auto',
+      width: 90,
+      height: 50
+    },
+    customError: {
+      color: '#FF0000',
+      fontSize: '0.7rem'
+    },
+    small: {
+      display: 'block',
+      marginTop: '1rem'
+    },
+    circularProgress: {
+      color: '#fff',
+      position: 'absolute'
+    },
+    paper: {
+      padding: 20
+    },
+    profile: {
+      '& .image-wrapper': {
+        textAlign: 'center',
+        position: 'relative',
+        '& button': {
+          position: 'absolute',
+          top: '80%',
+          left: '70%'
+        }
+      },
+      '& .profile-image': {
+        width: 200,
+        height: 200,
+        objectFit: 'cover',
+        maxWidth: '100%',
+        borderRadius: '50%'
+      },
+      '& .profile-details': {
+        textAlign: 'center',
+        '& span, svg': {
+          verticalAlign: 'middle'
+        },
+        '& a': {
+          color: '#00bcd4'
+        }
+      },
+      '& hr': {
+        border: 'none',
+        margin: '0 0 10px 0'
+      },
+      '& svg.button': {
+        '&:hover': {
+          cursor: 'pointer'
+        }
+      }
+    },
+    buttons: {
+      textAlign: 'center',
+      '& a': {
+        margin: '20px 10px'
+      }
+    }
   }
 }
