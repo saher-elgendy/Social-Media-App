ceaae5343590e5732bb7ee9dc245f4fd687d730e
unknown <saherelgendy1990@gmail.com>
2020-08-07 00:14:29 -0700

    refactor imports

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,14 +1,11 @@
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import makeStyles from '@material-ui/core/styles/makeStyles';
-import Typography from '@material-ui/core/Typography';
+import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+//dayjs
+import dayjs from 'dayjs';
+import calendar from 'dayjs/plugin/calendar';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-//dayjs
-import dayjs from 'dayjs';
-import calendar from 'dayjs/plugin/calendar'
 
 const useStyles = makeStyles({
     card: {
