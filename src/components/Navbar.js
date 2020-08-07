324643efe4825e207ec5ed0dff0890148a889978
unknown <saherelgendy1990@gmail.com>
2020-08-07 00:13:46 -0700

    connection Navbar to redux store

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
+
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import Toolbar from '@material-ui/core/toolbar';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { AppBar, Button, Toolbar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Add } from '@material-ui/icons';
 import { logout } from './../redux/actions/userActions';
+import ReusableButton from './reusable/ReusableButton';
+
 
 const useStyles = makeStyles({
     bar: {
@@ -19,24 +21,48 @@ const useStyles = makeStyles({
     }
 })
 
-const Navbar = ({ logout }) => {
+const Navbar = ({ authenticated, logout }) => {
+    console.log('authenticated', authenticated)
     const classes = useStyles();
-
     return (
         <AppBar className={classes.bar}>
             <Toolbar className="nav-container">
-                <Button color="inherit" component={Link} to="/">Home</Button>
-                <Button color="inherit" component={Link} to="/login">Login</Button>
-                <Button color="inherit" component={Link} to="/signup">Signup</Button>
-                <Button
-                    color="inherit"
-                    component={Link}
-                    to="/login"
-                    onClick={logout}
-                >Logout</Button>
+                {authenticated ? (
+                    <>
+                        <Button color="inherit" component={Link} to="/">Home</Button>
+                        <Button
+                            color="inherit"
+                            component={Link}
+                            to="/login"
+                            onClick={logout}
+                        >Logout</Button>
+
+                        <ReusableButton title="Post a scream">
+                            <Add color="primary" />
+                        </ReusableButton>
+                        <Button color="inherit" component={Link} to="/login">Login</Button>
+                            <Button color="inherit" component={Link} to="/signup">Signup</Button>
+                    </>
+                ) : (
+                        <>
+                            <Button color="inherit" component={Link} to="/login">Login</Button>
+                            <Button color="inherit" component={Link} to="/signup">Signup</Button>
+                           
+                        </>
+                    )
+                }
+
             </Toolbar>
         </AppBar>
     )
 }
 
-export default connect(null, { logout })(Navbar);
+Navbar.propTypes = {
+    logout: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    authenticated: state.user.authenticated
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
