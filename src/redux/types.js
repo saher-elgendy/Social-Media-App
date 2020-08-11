cbfab9316d495d7a5f53a7eeea7ae0fab22e3e30
unknown <saherelgendy1990@gmail.com>
2020-08-11 12:46:43 -0700

    adding data action types

--- src/redux/types.js
+++ src/redux/types.js
@@ -10,3 +10,7 @@ export const LOADING_UI = 'LOADING_UI';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 
 //data reducer types
+export const SET_SCREAMS = 'SET_SCRAMS';
+export const LOADING_DATA = 'LOADING_DATA';
+export const LIKE_SCREAM = 'LIKE_SCREAM';
+export const UNLIKE_SCREAM = 'UNLIKE_SCREAM';
