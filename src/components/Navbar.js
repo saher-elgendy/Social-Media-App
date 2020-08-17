c061a2866a1ce4ac16d72a6bfb5057a2c680fc30
unknown <saherelgendy1990@gmail.com>
2020-08-16 23:36:38 -0700

    showing authenticated navbar links for authenticated users

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,59 @@
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 import { AppBar, Button, Toolbar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Add } from '@material-ui/icons';
+import { Home, Notifications } from '@material-ui/icons';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { logout } from './../redux/actions/userActions';
 import ReusableButton from './reusable/ReusableButton';
+import PostScream from './PostScream';
 
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
+    ...theme.spread,
+    icon: {
+        color: '#fff'
+    },
+
     bar: {
-        display: 'flex',
         flexDirection: 'row',
         padding: 5,
+        color: '#fff'
     },
     img: {
         width: 80
     }
-})
+}));
 
 const Navbar = ({ authenticated, logout }) => {
-    console.log('authenticated', authenticated)
     const classes = useStyles();
+
     return (
         <AppBar className={classes.bar}>
             <Toolbar className="nav-container">
                 {authenticated ? (
                     <>
-                        <Button color="inherit" component={Link} to="/">Home</Button>
+                        <PostScream />
+                        <ReusableButton title="Home">
+                            <Home className={classes.icon} />
+                        </ReusableButton>
+                        <ReusableButton title="Notifications">
+                            <Notifications className={classes.icon} />
+                        </ReusableButton>
                         <Button
-                            color="inherit"
                             component={Link}
                             to="/login"
                             onClick={logout}
+                            className={classes.icon}
                         >Logout</Button>
-
-                        <ReusableButton title="Post a scream">
-                            <Add color="primary" />
-                        </ReusableButton>
-                        <Button color="inherit" component={Link} to="/login">Login</Button>
-                            <Button color="inherit" component={Link} to="/signup">Signup</Button>
                     </>
                 ) : (
                         <>
                             <Button color="inherit" component={Link} to="/login">Login</Button>
                             <Button color="inherit" component={Link} to="/signup">Signup</Button>
-                           
+
                         </>
                     )
                 }
@@ -64,5 +70,6 @@ Navbar.propTypes = {
 const mapStateToProps = state => ({
     authenticated: state.user.authenticated
 });
+
 
 export default connect(mapStateToProps, { logout })(Navbar);
