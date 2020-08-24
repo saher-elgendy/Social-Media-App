c6968f6e53505fa262a5517b6dc4f11884dad383
unknown <saherelgendy1990@gmail.com>
2020-08-23 20:43:25 -0700

    adding SET_SCREAM, SET_LOADING, SUBMIT_COMMENT and STOP_LOADING

--- src/redux/types.js
+++ src/redux/types.js
@@ -7,12 +7,16 @@ export const LOADING_USER = 'LOADING_USER';
 //UI reducer types
 export const SET_ERRORS = 'SET_ERRORS';
 export const LOADING_UI = 'LOADING_UI';
+export const STOP_LOADING = 'STOP_LOADING';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 
 //data reducer types
 export const SET_SCREAMS = 'SET_SCRAMS';
+export const SET_SCREAM = 'SET_SCREAM';
 export const POST_SCREAM = 'POST_SCREAM';
 export const LOADING_DATA = 'LOADING_DATA';
 export const LIKE_SCREAM = 'LIKE_SCREAM';
 export const UNLIKE_SCREAM = 'UNLIKE_SCREAM';
 export const DELETE_SCREAM = 'DELETE_SCREAM';
+export const SUBMIT_COMMENT = 'SUBMIT_COMMENT';
+export const SET_USER_SCREAMS = 'SET_USER_SCREAMS';
