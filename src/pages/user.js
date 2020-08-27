da7bc6610d6621ec2784a72973badc3099c5fc55
unknown <saherelgendy1990@gmail.com>
2020-08-26 20:05:12 -0700

    fix user component and implementing MUI style

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,35 +1,78 @@
+import { Grid, Typography } from '@material-ui/core';
+import PropTypes from 'prop-types';
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import Scream from '../components/Scream';
-import { getUserScreams } from '../redux/actions/dataActions';
-import PropTypes from 'prop-types';
+import { getUserProfile } from '../redux/actions/dataActions';
+import StaticProfile from '../components/StaticProfile';
+import { makeStyles } from '@material-ui/core/styles';
 
-const User = ({ getUserScreams, userScreams, ...props }) => {
+const useStyles = makeStyles({
+    screamsContainer: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between'
+    },
+    screamList: {
+        width: '100%'
+    },
+    typography: {
+        margin: '20px 0 0'
+    }
+})
+
+const User = ({ getUserProfile, screams, loading, ...props }) => {
+    const classes = useStyles();
     const userHandle = props.match.params.handle;
+    const screamId = props.match.params.screamId || null;
 
     useEffect(() => {
-        getUserScreams(userHandle)
+        getUserProfile(userHandle)
     }, []);
 
-    console.log('userScreams', userScreams)
     return (
-        <>
-            {
-                userScreams.map(scream => {
-                    return <Scream scream={scream} />
-                })
-            }
-        </>
+        !loading ?
+            <Grid container spacing={2}>
+                <Grid item xs={12} lg={7}>
+                    <div className={classes.screamsContainer}>
+                        <Typography color="primary" className={classes.typography}>
+                            All {userHandle} Screams
+                    </Typography>
+                        <Typography color="primary" className={classes.typography}>
+                            {screams.length}{' '}{screams.length === 1 ? 'scream' : 'screams'}
+                        </Typography>
+
+                        <ul className={classes.screamList}>
+                            {
+                                screams.map(scream => {
+                                    return scream.screamId !== screamId ?
+                                        <Scream key={scream.screamId} scream={scream} /> :
+                                        <Scream key={scream.screamId} scream={scream} openDialog />
+                                })
+                            }
+                        </ul>
+                    </div>
+
+                </Grid>
+                <Grid item xs={12} lg={5}>
+                    <StaticProfile />
+                </Grid>
+            </Grid> : 'Loading Profile...'
     )
 }
 
 User.propTypes = {
-    userScreams: PropTypes.array.isRequired,
-    getUserScreams: PropTypes.func.isRequired
+    getUserProfile: PropTypes.func.isRequired,
+    screams: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+
 }
 
-const mapStateToProps = state => ({
-    userScreams: state.data.userScreams
-});
-
-export default connect(mapStateToProps, { getUserScreams })(User);
+const mapStateToProps = state => {
+    const screams = state.data.userData.screams;
+    return {
+        screams: screams ? screams : [],
+        loading: state.data.loading
+    }
+}
+export default connect(mapStateToProps, { getUserProfile })(User);
