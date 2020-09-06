baa5670925defbef4a7e47f25d65827a2361791e
unknown <saherelgendy1990@gmail.com>
2020-09-05 22:42:55 -0700

    adding login message for unauthenticated users

--- src/pages/user.js
+++ src/pages/user.js
@@ -6,6 +6,7 @@ import Scream from '../components/Scream';
 import { getUserProfile } from '../redux/actions/dataActions';
 import StaticProfile from '../components/StaticProfile';
 import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles({
     screamsContainer: {
@@ -13,15 +14,24 @@ const useStyles = makeStyles({
         flexWrap: 'wrap',
         justifyContent: 'space-between'
     },
+    noScreamsContainer: {
+        margin: '50px auto'
+    },
     screamList: {
         width: '100%'
     },
     typography: {
         margin: '20px 0 0'
+    },
+    loginMessage: {
+        width: '100%',
+        height: '100%',
+        textAlign: 'center',
+        marginTop: '20%',
     }
 })
 
-const User = ({ getUserProfile, screams, loading, ...props }) => {
+const User = ({ getUserProfile, screams, loading, authenticated, ...props }) => {
     const classes = useStyles();
     const userHandle = props.match.params.handle;
     const screamId = props.match.params.screamId || null;
@@ -29,35 +39,46 @@ const User = ({ getUserProfile, screams, loading, ...props }) => {
     useEffect(() => {
         getUserProfile(userHandle)
     }, []);
-
+    console.log(loading, authenticated)
     return (
-        !loading ?
-            <Grid container spacing={2}>
-                <Grid item xs={12} lg={7}>
-                    <div className={classes.screamsContainer}>
-                        <Typography color="primary" className={classes.typography}>
-                            All {userHandle} Screams
-                    </Typography>
-                        <Typography color="primary" className={classes.typography}>
-                            {screams.length}{' '}{screams.length === 1 ? 'scream' : 'screams'}
-                        </Typography>
-
-                        <ul className={classes.screamList}>
+        authenticated ?
+            (!loading ?
+                <Grid container spacing={2}>
+                    <Grid item xs={12} lg={7}>
+                        <div className={classes.screamsContainer}>
                             {
-                                screams.map(scream => {
-                                    return scream.screamId !== screamId ?
-                                        <Scream key={scream.screamId} scream={scream} /> :
-                                        <Scream key={scream.screamId} scream={scream} openDialog />
-                                })
+                                screams.length ?
+                                    <>
+                                        <Typography color="primary" className={classes.typography}>
+                                            All {userHandle} Screams
+                                    </Typography>
+                                        <Typography color="primary" className={classes.typography}>
+                                            {screams.length}{' '}{screams.length === 1 ? 'scream' : 'screams'}
+                                        </Typography>
+                                    </> :
+                                    <Typography className={classes.noScreamsContainer}>
+                                        No screams by this user
+                                </Typography>
                             }
-                        </ul>
-                    </div>
 
-                </Grid>
-                <Grid item xs={12} lg={5}>
-                    <StaticProfile />
-                </Grid>
-            </Grid> : 'Loading Profile...'
+                            <ul className={classes.screamList}>
+                                {
+                                    screams.map(scream => {
+                                        return scream.screamId !== screamId ?
+                                            <Scream key={scream.screamId} scream={scream} /> :
+                                            <Scream key={scream.screamId} scream={scream} openDialog />
+                                    })
+                                }
+                            </ul>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} lg={5}>
+                        <StaticProfile />
+                    </Grid>
+                </Grid> : 'Loading Profile...') : (
+                <Typography className={classes.loginMessage}>
+                    Please <Link to="/login">login</Link> to see this user profile and activity
+                </Typography>)
     )
 }
 
@@ -65,14 +86,14 @@ User.propTypes = {
     getUserProfile: PropTypes.func.isRequired,
     screams: PropTypes.array.isRequired,
     loading: PropTypes.bool.isRequired
-
 }
 
 const mapStateToProps = state => {
     const screams = state.data.userData.screams;
     return {
-        screams: screams ? screams : [],
-        loading: state.data.loading
+        screams: screams || [],
+        loading: state.data.loading,
+        authenticated: state.user.authenticated
     }
 }
 export default connect(mapStateToProps, { getUserProfile })(User);
