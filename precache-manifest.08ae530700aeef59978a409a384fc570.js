160c57a93d2a6a3222763872cd02fa8473651d03
unknown <saherelgendy1990@gmail.com>
2020-09-17 22:21:48 -0700

    Updates

---
+++ precache-manifest.08ae530700aeef59978a409a384fc570.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "986525585ef6f96124f21620b15efc2b",
+    "url": "/Social-Media-App/index.html"
+  },
+  {
+    "revision": "8d7a33ad3e568b6f0e60",
+    "url": "/Social-Media-App/static/css/main.ed3fcef7.chunk.css"
+  },
+  {
+    "revision": "4a663861028f11404270",
+    "url": "/Social-Media-App/static/js/2.56532926.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/Social-Media-App/static/js/2.56532926.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d7a33ad3e568b6f0e60",
+    "url": "/Social-Media-App/static/js/main.310ff7bb.chunk.js"
+  },
+  {
+    "revision": "24c874208e4873b57aba",
+    "url": "/Social-Media-App/static/js/runtime-main.0ac16bb0.js"
+  },
+  {
+    "revision": "bb5d4fa12d1cf3b123b4f84157dddb14",
+    "url": "/Social-Media-App/static/media/icon.bb5d4fa1.png"
+  }
+]);
